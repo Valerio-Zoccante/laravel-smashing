28597b71af3ab9a2cfc581afdab3e91619cdc783
Valerio Zoccante <valeriozoc@gmail.com>
2020-05-07 10:46:24 +0200

    structure-initiated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,15 @@
-<header>
-
-</header>
+<div class="header">
+    <div class="logo">
+        <img src="https://d33wubrfki0l68.cloudfront.net/3fbf6ef77e5e00aea7a22423087373169b52ae7e/413ff/images/logo/logo.svg" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Guides</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Membership</a></li>
+            <li><a href="#">Shops</a></li>
+        </ul>
+    </nav>
+</div>
